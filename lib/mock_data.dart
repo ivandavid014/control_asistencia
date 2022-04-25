f6c0e65e638adb73f5e5a7db6ab312ba3ed6281a
IvanDavid <ivan.david@live.com.ar>
2022-04-25 13:23:46 -0300

    dependencies agregadas y mockito generado

---
+++ lib/mock_data.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<DatosEmpl> fetchDatosEmpl(http.Client client) async {
+  final response = await client
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return DatosEmpl.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class DatosEmpl {
+  static DatosEmpl fromJson(Map<String, dynamic> async) {
+    return DatosEmpl();
+  }
+}
---
+++ test/mock_data_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+
+@GenerateMocks([http.Client])
+void main() {
+  testWidgets('mock data ...', (tester) async {
+    // TODO: Implement test
+  });
+}
