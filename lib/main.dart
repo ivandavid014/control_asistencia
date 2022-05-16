1e2f8877c27eedea8d8df0f9078d44d49ecb1310
Ivan <ivaan.148@gmail.com>
2022-05-16 01:05:36 -0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:control_personal_municipal/database.dart';
 import 'package:control_personal_municipal/empleadoDTO.dart';
 import 'package:flutter/material.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'empleado_page.dart';
 
-void main() => runApp(MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
 
 List<Empleado> empleados =
     empleadosEjemplo.map((dynamic item) => Empleado.fromJson(item)).toList();
